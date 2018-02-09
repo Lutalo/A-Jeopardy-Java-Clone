e4c0c5a98da654d91abe503ed27c856af90600a2
Lutalo <15132571+Lutalo@users.noreply.github.com>
2018-02-09 11:02:34 -0800

    Added Board, Column, and AnswerCard classes
    getters and setters for answercard

---
+++ src/AnswerCard.java
@@ -0,0 +1,25 @@
+
+public class AnswerCard
+{
+    private String display;
+    private String content;
+    
+    public String getDisplay()
+    {
+        return display;
+    }
+    public void setDisplay(String display)
+    {
+        this.display = display;
+    }
+    public String getContent()
+    {
+        return content;
+    }
+    public void setContent(String content)
+    {
+        this.content = content;
+    }
+    
+    
+}
