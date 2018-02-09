83e4e781f18a0834172147e6e5e657b1f92d318e
Lutalo <15132571+Lutalo@users.noreply.github.com>
2018-02-09 15:28:56 -0800

    Finished AnswerCard class

--- src/AnswerCard.java
+++ src/AnswerCard.java
@@ -3,6 +3,14 @@ public class AnswerCard
 {
     private String display;
     private String content;
+    private int amount;
+    
+    public AnswerCard(String display, String content, int amount)
+    {
+	this.display = display;
+	this.content = content;
+	this.amount = amount;
+    }
     
     public String getDisplay()
     {
